87bc98baa849a9874c453c332b9c25289564fa43
Fr-yayou <yanis.bekhtaoui@gmail.com>
2020-11-25 02:19:18 -0800

    store set

--- twp_frontend/src/store/modules/trello.js
+++ twp_frontend/src/store/modules/trello.js
@@ -1,12 +1,23 @@
-// import axios from "axios"
+import axios from "axios"
 
 const state = {
+    trello:[],
+    user :[]
+};
+
+const actions= {
+
+    async fetchCards({commit}){
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        console.log(response.data)
+        commit("setCards", response.data)
+    }
 };
 const mutations= {
-};
-const actions= {
+    setCards:(state,list) => (state.trello = list)
 };
 const getters= {
+    allCards:(state) => state.trello
 };
 
 export default{
--- twp_frontend/src/views/Home.vue
+++ twp_frontend/src/views/Home.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="home">
+    <div v-for="card in allCards" :key="card.id">
+      {{card.id}}
+    </div>
   </div>
 </template>
 
 <script>
-
+import {mapGetters,mapActions} from "vuex"
 export default {
   name: 'Home',
   components: {
+  },
+
+  computed:{
+    ...mapGetters(["allCards"])
+  },
+
+  methods:{
+    ...mapActions(["fetchCards"])
+  },
+
+  created (){
+    this.fetchCards()
   }
+
+
+
 }
 </script>
