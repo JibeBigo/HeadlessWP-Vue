f4722e668a93a4d8d7866f3a1d936f12f208d552
YoannP-08 <yoann.pons08@gmail.com>
2020-11-25 09:03:05 +0100

    Clean-up vuejs initial project

--- twp_frontend/src/views/Home.vue
+++ twp_frontend/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CardItem />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CardItem from '@/components/CardItem.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    CardItem,
   }
 }
 </script>
