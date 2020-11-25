22467c543ccd4893a14b953f914c8f9b847b87fc
YoannP-08 <yoann.pons08@gmail.com>
2020-11-25 09:00:08 +0100

    [ADD] CardItem components to display card title

---
+++ twp_frontend/src/components/CardItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-card
+        class="m-4 rounded-tl-xl rounded-br-xl"
+        elevation="24"
+        outlined
+    >
+        <v-card-title>Test</v-card-title>
+        <v-card-text>
+            <v-icon> {{ icons.mdiPlaylistCheck}} </v-icon>
+        </v-card-text>
+
+    </v-card>
+</template>
+
+<script>
+import { mdiPlaylistCheck } from '@mdi/js'
+
+export default {
+    name: "CardItem",
+    data () {
+        return {
+            icons: { mdiPlaylistCheck },
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
+
