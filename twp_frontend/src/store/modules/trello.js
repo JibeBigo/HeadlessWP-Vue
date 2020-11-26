1feb9e0d24f587b587a89c6cc2ca9b3e150a7c84
Bigo <jbsilvestre@hotmail.fr>
2020-11-26 13:42:47 +0100

    all

--- twp_frontend/src/store/modules/trello.js
+++ twp_frontend/src/store/modules/trello.js
@@ -9,20 +9,20 @@ const state = {
 const actions = {
   async fetchListItems({ commit }) {
     const response = await axios.get(
-      "http://localhost:8000/?rest_route=/wp/v2/categories",
+      "http://localhost:8000/wp-json/wp/v2/categories",
     );
     commit("setListItems", response.data);
   },
 
   async fetchUsers({ commit }) {
     const response = await axios.get(
-      "http://localhost:8000/?rest_route=/wp/v2/users/",
+      "http://localhost:8000/wp-json/wp/v2/users/",
       {
         auth: {
           username: "jb",
           password: "GBRwoADbd9IiTfqazYwI$vFw",
         },
-    },
+      },
     );
     console.log(response.data);
     commit("setUsers", response.data);
@@ -44,7 +44,7 @@ const actions = {
 const mutations = {
   setListItems: (state, list) => (state.listItems = list),
   setUsers: (state, user) => (state.users = user),
-}
+};
 const getters = {
   allListItems: (state) => state.listItems,
   allUsers: (state) => state.users,
