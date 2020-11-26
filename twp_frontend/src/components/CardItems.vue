2b45e8a8288a674b2994b68429d7a0f316d822eb
YoannP-08 <yoann.pons08@gmail.com>
2020-11-26 13:41:21 +0100

    [ADD] dummy template for card details pop-up + [MOD] route in store

--- twp_frontend/src/components/CardItems.vue
+++ twp_frontend/src/components/CardItems.vue
@@ -6,21 +6,44 @@
             width="600"
         >
             <template v-slot:activator="{ on, attrs }">
-                    <v-card
-                        class="mb-4 rounded-tl-xl rounded-br-xl"
-                        elevation="10"
-                        outlined
-                        v-bind="attrs"
-                        v-on="on"
-                        width="220"
-                    >
-                        <v-card-title> {{ card.title.rendered }} </v-card-title>
-                        <v-card-text> 
-                            <v-icon v-if="card.content.rendered"> {{ icons.mdiPlaylistCheck }} </v-icon>
-                            <v-icon v-else></v-icon>
-                        </v-card-text>
-                    </v-card>
+                <v-card
+                    class="mb-4 rounded-tl-xl rounded-br-xl"
+                    elevation="5"
+                    outlined
+                    v-bind="attrs"
+                    v-on="on"
+                    width="220"
+                >
+                    <v-card-title> {{ card.title.rendered }} </v-card-title>
+                    <v-card-text> 
+                        <v-icon v-if="card.content.rendered"> {{ icons.mdiPlaylistCheck }} </v-icon>
+                        <v-icon v-else></v-icon>
+                    </v-card-text>
+                </v-card>
             </template>
+
+            <v-card>
+            <v-card-title class="headline grey lighten-2">
+            Privacy Policy
+            </v-card-title>
+
+            <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                color="primary"
+                text
+                @click="dialog = false"
+            >
+                I accept
+            </v-btn>
+            </v-card-actions>
+      </v-card>
         </v-dialog>
     </div>
 </template>
--- twp_frontend/src/store/modules/trello.js
+++ twp_frontend/src/store/modules/trello.js
@@ -15,18 +15,18 @@ const getters= {
 const actions= {
 
     async fetchListItems({commit}){
-        const response = await axios.get('http://localhost:8000/?rest_route=/wp/v2/categories')
+        const response = await axios.get('http://localhost:8000/wp-json/wp/v2/categories')
         commit("setListItems", response.data)
     },
 
     async fetchCards({ commit }) {
-        const response = await axios.get('http://localhost:8000/?rest_route=/wp/v2/posts')
+        const response = await axios.get('http://localhost:8000/wp-json//wp/v2/posts')
         commit("setCards", response.data)
     },
 
     async addCard ({ commit }, title) {
         let response = await axios.post(
-            'http://localhost:8000/?rest_route=/wp/v2/posts',
+            'http://localhost:8000/wp-json//wp/v2/posts',
             title, { headers:{'Authorization' : `Basic ${tokenY}`}}
         );
         console.log(response.data)
