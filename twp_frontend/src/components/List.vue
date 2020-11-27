a4532f2411ebdfc241dbbe3ebd53fc77148f7723
YoannP-08 <yoann.pons08@gmail.com>
2020-11-27 15:18:19 +0100

    [MOD] patched errors edit title list via input

--- twp_frontend/src/components/List.vue
+++ twp_frontend/src/components/List.vue
@@ -1,74 +1,73 @@
 <template>
-  <div class="container-card">
-    <v-card class="mr-2 blue-grey lighten-5" width="272" min-height="100">
-      <div class="d-flex justify-space-between align-center">
-        <v-card-title class="pointer py-0">
-          <input
-            ref="input"
-            v-on:keyup.enter="updateTitleList(list)"
-            @click="inputStyle"
-            @blur="onCloseInput"
-            v-bind:class="{updateFocus:input}"
-            class="textarea-update"
-            type="text"
-            v-model="newListTitle"
-            v-bind:placeholder="list.name"
-          />
-        </v-card-title>
-        <v-btn v-click-outside="toggleCloseModal" @click="toggleModal" icon>
-          <v-icon>{{icons.mdiDotsHorizontal}}</v-icon>
-        </v-btn>
-      </div>
-      <div v-if="modalList" class="modal">
-        <v-card>
-          <v-card-title class="d-flex justify-center">
-            <span>List Actions</span>
-            <v-btn class="modalCorner" @click="toggleCloseModal" icon>
-              <v-icon>{{icons.mdiClose}}</v-icon>
+    <div class="container-card">
+        <v-card class="mr-2 blue-grey lighten-5" width="272" min-height="100">
+        <div class="d-flex justify-space-between align-center">
+            <v-card-title class="pointer py-0">
+            <input
+                ref="input"
+                v-on:keyup.enter="updateTitleList(list)"
+                @click="inputStyle"
+                @blur="onCloseInput"
+                v-bind:class="{updateFocus:input}"
+                class="textarea-update"
+                type="text"
+                v-model="newListTitle"
+            />
+            </v-card-title>
+            <v-btn v-click-outside="toggleCloseModal" @click="toggleModal" icon>
+            <v-icon>{{icons.mdiDotsHorizontal}}</v-icon>
             </v-btn>
-          </v-card-title>
-          <v-divider class="ml-5 mr-5"></v-divider>
-          <v-card-text>
-            <v-list>
-              <div class="container-deleteList">
-                <p class="deleteListAction">Delete List</p>
-                <v-btn @click="cancelList(list.id)" icon color="red lighten-1">
-                  <v-icon>{{icons.mdiCloseCircleOutline}}</v-icon>
+        </div>
+        <div v-if="modalList" class="modal">
+            <v-card>
+            <v-card-title class="d-flex justify-center">
+                <span>List Actions</span>
+                <v-btn class="modalCorner" @click="toggleCloseModal" icon>
+                <v-icon>{{icons.mdiClose}}</v-icon>
                 </v-btn>
-              </div>
-            </v-list>
-          </v-card-text>
-        </v-card>
-      </div>
-   
-    <!-- <v-list-cards>SPOT FOR THE COMMENT CARD BODY</v-list-cards> -->
-    <div v-for="card in allCards" :key="card.id">
-      <v-list-item v-if="card.categories[0] === list.id" class="mx-auto px-2">
-        <CardItems v-bind:card="card" v-bind:list="list" />
-      </v-list-item>
-    </div>
+            </v-card-title>
+            <v-divider class="ml-5 mr-5"></v-divider>
+            <v-card-text>
+                <v-list>
+                <div class="container-deleteList">
+                    <p class="deleteListAction">Delete List</p>
+                    <v-btn @click="cancelList(list.id)" icon color="red lighten-1">
+                    <v-icon>{{icons.mdiCloseCircleOutline}}</v-icon>
+                    </v-btn>
+                </div>
+                </v-list>
+            </v-card-text>
+            </v-card>
+        </div>
+    
+        <!-- <v-list-cards>SPOT FOR THE COMMENT CARD BODY</v-list-cards> -->
+        <div v-for="card in allCards" :key="card.id">
+        <v-list-item v-if="card.categories[0] === list.id" class="mx-auto px-2">
+            <CardItems v-bind:card="card" v-bind:list="list" />
+        </v-list-item>
+        </div>
 
-    <div class="addCard">
-      <v-btn v-bind:class="{none:formOn}" @click="toggle" depressed>
-        <v-icon size="15">mdi-plus</v-icon>Add another card
-      </v-btn>
-      <div v-if="formOn">
-        <v-form @submit="onSubmit">
-          <v-card width="250">
-            <v-textarea
-              v-model="cardTitle"
-              class="mr-5 ml-5"
-              placeholder="Enter a title for this card..."
-              rows="2"
-            ></v-textarea>
-            <v-btn color="green lighten-1" type="submit">Add Card</v-btn>
-            <v-btn @click="toggle" icon>X</v-btn>
-          </v-card>
-        </v-form>
-      </div>
+        <div class="addCard">
+        <v-btn v-bind:class="{none:formOn}" @click="toggle" depressed>
+            <v-icon size="15">mdi-plus</v-icon>Add another card
+        </v-btn>
+        <div v-if="formOn">
+            <v-form @submit="onSubmit">
+            <v-card width="250">
+                <v-textarea
+                v-model="cardTitle"
+                class="mr-5 ml-5"
+                placeholder="Enter a title for this card..."
+                rows="2"
+                ></v-textarea>
+                <v-btn color="green lighten-1" type="submit">Add Card</v-btn>
+                <v-btn @click="toggle" icon>X</v-btn>
+            </v-card>
+            </v-form>
+        </div>
+        </div>
+        </v-card>
     </div>
-    </v-card>
-  </div>
 </template>
 
 <script>
@@ -76,87 +75,88 @@ import { mdiDotsHorizontal, mdiClose, mdiCloseCircleOutline } from "@mdi/js";
 import CardItems from "@/components/CardItems.vue";
 import { mapGetters, mapActions } from "vuex";
 import ClickOutside from "vue-click-outside";
+
 export default {
-  name: "List",
-  props: ["list"],
-  data() {
-    return {
-      icons: { mdiDotsHorizontal, mdiClose, mdiCloseCircleOutline },
-      modalList: false,
-      cardTitle: "",
-      formOn: false,
-      newListTitle: "",
-      input: false,
-    };
-  },
-  components: { CardItems },
+    name: "List",
+    props: ["list"],
+    data() {
+        return {
+        icons: { mdiDotsHorizontal, mdiClose, mdiCloseCircleOutline },
+        modalList: false,
+        cardTitle: "",
+        formOn: false,
+        newListTitle: this.list.name,
+        input: false,
+        };
+    },
+    components: { CardItems },
 
-  methods: {
-    ...mapActions(["deleteList", "fetchCards", "addCard", "updateList"]),
-    toggleModal() {
-        this.modalList = !this.modalList
-    },
-    toggleCloseModal() {
-      this.modalList = false;
-    },
-    toggle() {
-      this.formOn = !this.formOn;
-    },
+    methods: {
+        ...mapActions(["deleteList", "fetchCards", "addCard", "updateList"]),
+        toggleModal() {
+            this.modalList = !this.modalList
+        },
+        toggleCloseModal() {
+        this.modalList = false;
+        },
+        toggle() {
+        this.formOn = !this.formOn;
+        },
 
-    //function to open input when cliking
-    inputStyle() {
-      this.input = true;
-    },
-    //function to close input when clicking outside
-    onCloseInput() {
-      this.input = false;
-    },
+        //function to open input when cliking
+        inputStyle() {
+        this.input = true;
+        },
+        //function to close input when clicking outside
+        onCloseInput() {
+        this.input = false;
+        },
 
-    cancelList(id) {
-      console.log(id);
-      this.deleteList(id);
-    },
+        cancelList(id) {
+        console.log(id);
+        this.deleteList(id);
+        },
 
-    onSubmit(event) {
-      event.preventDefault();
-      let newCard = {
-        title: this.cardTitle,
-        status: "publish",
-        content: "",
-        categories: this.list.id,
-      };
-      console.log(newCard);
-      this.addCard(newCard);
-      this.cardTitle = "";
-    },
+        onSubmit(event) {
+        event.preventDefault();
+        let newCard = {
+            title: this.cardTitle,
+            status: "publish",
+            content: "",
+            categories: this.list.id,
+        };
+        console.log(newCard);
+        this.addCard(newCard);
+        this.cardTitle = "";
+        },
 
-    updateTitleList(list) {
-      event.preventDefault();
-      const id = list.id;
-      console.log(id, "id");
-      let updateList = {
-        id: list.id,
-        description: list.description,
-        name: this.newListTitle,
-        slug: list.slug,
-        parent: list.parent,
-        meta: list.meta,
-      };
-      this.updateList({ updateList, id });
+        updateTitleList(list) {
+        event.preventDefault();
+        const id = list.id;
+        console.log(id, "id");
+        let updateList = {
+            id: list.id,
+            description: list.description,
+            name: this.newListTitle,
+            slug: list.slug,
+            parent: list.parent,
+            meta: list.meta,
+        };
+        this.updateList({ updateList, id });
+        },
     },
-  },
-  computed: {
-    ...mapGetters(["allCards"]),
-  },
-  created() {
-    this.fetchCards();
-  },
-  mounted() {
-    this.popupItem = this.$el;
-  },
-  directives: {
-    ClickOutside,
-  },
+    computed: {
+        ...mapGetters(["allCards"]),
+    },
+    created() {
+        this.fetchCards();
+    },
+    mounted() {
+        this.popupItem = this.$el;
+    },
+    directives: {
+        ClickOutside,
+    },
 };
 </script>
 
@@ -195,12 +195,13 @@ export default {
   height: 25px;
   outline: none;
   text-indent: 5px;
-  border: 1px solid white;
-  background-color: white;
+  border:1px solid #ecefe1;
+ background-color:#ecefe1
 }
 .updateFocus {
-  border: 2px solid #1e88e5;
-  border-radius: 2px;
+    background-color: white;
+    border: 2px solid #1e88e5;
+    border-radius: 2px;
 }
 ::placeholder {
   font-size: 15px;
--- twp_frontend/src/store/modules/trello.js
+++ twp_frontend/src/store/modules/trello.js
@@ -2,146 +2,146 @@ import axios from "axios"
 const token ="eW9hbm4ucG9uc0BlcGl0ZWNoLmV1Ok1vaSZUb2kwOHdz"
 
 const state = {
-  listItems: [],
-  cards: [],
-  user: [],
-  users: [],
-  comments: [],
+    listItems: [],
+    cards: [],
+    user: [],
+    users: [],
+    comments: [],
 };
 
 const getters = {
-  allListItems: (state) => state.listItems,
-  allCards: (state) => state.cards,
-  allUsers: (state) => state.users,
-  allComments: (state) => state.comments,
+    allListItems: (state) => state.listItems,
+    allCards: (state) => state.cards,
+    allUsers: (state) => state.users,
+    allComments: (state) => state.comments,
 };
 
 const actions = {
-  async fetchComments({ commit }) {
-    const response = await axios.get(
-      "http://localhost:8000/wp-json/wp/v2/comments?_fields=author_name,post,content,date,id",
-    );
-    commit("setComments", response.data);
-  },
+    async fetchComments({ commit }) {
+        const response = await axios.get(
+        "http://localhost:8000/wp-json/wp/v2/comments?_fields=author_name,post,content,date,id",
+        );
+        commit("setComments", response.data);
+    },
 
-  async addComment({ commit }, comment) {
-    const response = await axios.post(
-      "http://localhost:8000/wp-json/wp/v2/comments",
-      comment,
-      {
-        headers: { Authorization: `Basic ${token}` },
-      },
-    );
-    commit("newComment", response.data);
-  },
-
-  async deleteComment({ commit }, id) {
-    const response = await axios.delete(
-      `http://localhost:8000/wp-json/wp/v2/comments/${id}?force=true`,
-      {
-        headers: { Authorization: `Basic ${token}` },
-      },
-    );
-    commit("removeComment", response.data.previous.id);
-  },
-
-  async fetchUsers({ commit }) {
-    const response = await axios.get(
-      "http://localhost:8000/wp-json/wp/v2/users/",
-      {
-        headers: { Authorization: `Basic ${token}` },
-      },
-    );
-    commit("setUsers", response.data);
-  },
-  //   async createUser({ commit }, user) {
-  //     var response = await axios.post(
-  //       "http://localhost:8000/?rest_route=/wp/v2/users",
-  //       {
-  //         name: user.name,
-  //         email: user.email,
-  //         password: user.password,
-  //       },
-  //     );
-  //     commit("newUser", response.data.note);
-  //     // const newNote = actions.fetchNote(response.data.note._id);
-  //     // commit("newNote", newNote);
-  //   },
-  async fetchListItems({ commit }) {
-    const response = await axios.get(
-      "http://localhost:8000/wp-json/wp/v2/categories",
-    );
-    commit("setListItems", response.data);
-  },
-
-  async addList({ commit }, newList) {
-    let response;
-    response = await axios
-      .post("http://localhost:8000/wp-json/wp/v2/categories", newList, {
-        headers: { Authorization: `Basic ${token}` },
-      })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    console.log(response);
-
-    commit("newList", newList);
-  },
-
-  async deleteList({ commit }, id) {
-    await axios
-      .delete(
-        `http://localhost:8000/wp-json/wp/v2/categories/${id}?force=true`,
+    async addComment({ commit }, comment) {
+        const response = await axios.post(
+        "http://localhost:8000/wp-json/wp/v2/comments",
+        comment,
         {
-          headers: { Authorization: `Basic ${token}` },
+            headers: { Authorization: `Basic ${token}` },
         },
-      )
-      .then((response) => {
+        );
+        commit("newComment", response.data);
+    },
+
+    async deleteComment({ commit }, id) {
+        const response = await axios.delete(
+        `http://localhost:8000/wp-json/wp/v2/comments/${id}?force=true`,
+        {
+            headers: { Authorization: `Basic ${token}` },
+        },
+        );
+        commit("removeComment", response.data.previous.id);
+    },
+
+    async fetchUsers({ commit }) {
+        const response = await axios.get(
+        "http://localhost:8000/wp-json/wp/v2/users/",
+        {
+            headers: { Authorization: `Basic ${token}` },
+        },
+        );
+        commit("setUsers", response.data);
+    },
+    //   async createUser({ commit }, user) {
+    //     var response = await axios.post(
+    //       "http://localhost:8000/?rest_route=/wp/v2/users",
+    //       {
+    //         name: user.name,
+    //         email: user.email,
+    //         password: user.password,
+    //       },
+    //     );
+    //     commit("newUser", response.data.note);
+    //     // const newNote = actions.fetchNote(response.data.note._id);
+    //     // commit("newNote", newNote);
+    //   },
+    async fetchListItems({ commit }) {
+        const response = await axios.get(
+        "http://localhost:8000/wp-json/wp/v2/categories",
+        );
+        commit("setListItems", response.data);
+    },
+
+    async addList({ commit }, newList) {
+        let response;
+        response = await axios
+        .post("http://localhost:8000/wp-json/wp/v2/categories", newList, {
+            headers: { Authorization: `Basic ${token}` },
+        })
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
         console.log(response);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
 
-    commit("removeList", id);
-  },
+        commit("newList", newList);
+    },
 
-  async updateList({ commit }, { updateList, id }) {
-    console.log(updateList);
-    console.log(id);
-    let response = await axios.post(
-      `http://localhost:8000/wp-json/wp/v2/categories/${id}`,
-      updateList,
-      {
-        headers: { Authorization: `Basic ${token}` },
-      },
-    );
-    console.log(response);
+    async deleteList({ commit }, id) {
+        await axios
+        .delete(
+            `http://localhost:8000/wp-json/wp/v2/categories/${id}?force=true`,
+            {
+            headers: { Authorization: `Basic ${token}` },
+            },
+        )
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 
-    commit("freshList", updateList);
-  },
+        commit("removeList", id);
+    },
 
-  async fetchCards({ commit }) {
-    const response = await axios.get(
-      "http://localhost:8000/wp-json//wp/v2/posts",
-    );
-    commit("setCards", response.data);
-  },
+    async updateList({ commit }, { updateList, id }) {
+        console.log(updateList);
+        console.log(id);
+        let response = await axios.post(
+        `http://localhost:8000/wp-json/wp/v2/categories/${id}`,
+        updateList,
+        {
+            headers: { Authorization: `Basic ${token}` },
+        },
+        );
+        console.log(response);
 
-  async addCard({ commit }, title) {
-    let response = await axios.post(
-      "http://localhost:8000/wp-json//wp/v2/posts",
-      title,
-      { headers: { Authorization: `Basic ${token}` } },
-    );
-    console.log(response.data);
-    commit("newCard", response.data);
-  },
+        commit("freshList", updateList);
+    },
 
-  async deleteCard({ commit }, id) {
+    async fetchCards({ commit }) {
+        const response = await axios.get(
+        "http://localhost:8000/wp-json//wp/v2/posts",
+        );
+        commit("setCards", response.data);
+    },
+
+    async addCard({ commit }, title) {
+        let response = await axios.post(
+        "http://localhost:8000/wp-json//wp/v2/posts",
+        title,
+        { headers: { Authorization: `Basic ${token}` } },
+        );
+        console.log(response.data);
+        commit("newCard", response.data);
+    },
+
+    async deleteCard({ commit }, id) {
     await axios.delete(`http://localhost:8000/wp-json//wp/v2/posts/${id}?force=true`, {
     headers: { 'Authorization': `Basic ${token}` }
 });
@@ -150,23 +150,23 @@ const actions = {
 };
 
 const mutations = {
-  setListItems: (state, list) => (state.listItems = list),
-  newList: (state, newList) => state.listItems.push(newList),
-  removeList: (state, id) =>
-    (state.listItems = state.listItems.filter((list) => list.id != id)),
-  setCards: (state, cardItems) => (state.cards = cardItems),
-  newCard: (state, newCard) => state.cards.unshift(newCard),
-  removeCard:(state,id) => state.cards = state.cards.filter(card => card.id != id),
-  setUsers: (state, users) => (state.users = users),
-  setComments: (state, comments) => (state.comments = comments),
-  newComment: (state, comment) => state.comments.unshift(comment),
-  removeComment: (state, id) =>
-    (state.comments = state.comments.filter((comment) => comment.id != id)),
+    setListItems: (state, list) => (state.listItems = list),
+    newList: (state, newList) => state.listItems.push(newList),
+    removeList: (state, id) =>
+        (state.listItems = state.listItems.filter((list) => list.id != id)),
+    setCards: (state, cardItems) => (state.cards = cardItems),
+    newCard: (state, newCard) => state.cards.unshift(newCard),
+    removeCard:(state,id) => state.cards = state.cards.filter(card => card.id != id),
+    setUsers: (state, users) => (state.users = users),
+    setComments: (state, comments) => (state.comments = comments),
+    newComment: (state, comment) => state.comments.unshift(comment),
+    removeComment: (state, id) =>
+        (state.comments = state.comments.filter((comment) => comment.id != id)),
 };
 
 export default {
-  state,
-  getters,
-  actions,
-  mutations,
+    state,
+    getters,
+    actions,
+    mutations,
 };
